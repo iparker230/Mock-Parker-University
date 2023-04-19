8826211f88e4c6ed526efd31f47ae41ab06919d6
iparker230 <115053225+iparker230@users.noreply.github.com>
2023-04-19 12:18:27 -0500

    code correction

--- form-manager.php
+++ form-manager.php
@@ -4,23 +4,21 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'paruni1876@pui.edu';
+$email_from = 'uni1876@pui.edu';
 
-$email_subject = 'New Form Submission'
+$email_subject = 'New Form Submission';
 
-$email_body = "Username: $name.\n".
+$email_body = "Username: <a href='".$name."'>$name</a>\n".
                 "User Email: $visitor_email.\n". 
                   "Subject: $subject.\n". 
                   "User Message: $message.\n";
 
-$to = 'imaniparker2@gmail.com'
+$to = 'parkeruni1876@gmail.com';
 
 $headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
-$header . = "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers)
+mail($to,$email_subject,$email_body,$headers);
 
 header("Location: contact.html");
 ?>
-
